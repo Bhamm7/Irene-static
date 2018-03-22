70e4bbd111d40379e79fa4b72c42542e7cc9ceeb
MEGANET\Brett.Hamm <brett.hamm@megasys.com>
2018-03-22 17:09:08 -0600

    reworked site, added navbar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,48 @@
 import React from 'react'
 import { Router, Link } from 'react-static'
 import { hot } from 'react-hot-loader'
+import { ParallaxProvider } from 'react-scroll-parallax'
+import ResponsiveMenu from 'react-responsive-navbar'
 //
 import Routes from 'react-static-routes'
+import menuImg from 'images/logo.png'
+
 
 import './app.scss'
 
 const App = () => (
+  <ParallaxProvider>
     <Router>
-     
-        
-        <div className="wrapper">
+      <div>
         <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/social">Social</Link>
-          <Link to="/gallery">Gallery</Link>
-          <Link to="/workshops">Workshops</Link>
-          <Link to="/store">Store</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/blog">Blog</Link>
+          <div className="logo">
+            IRENE RUDNYK PHOTOGRAPHY
+          </div>
+          <ResponsiveMenu
+            menuOpenButton={<div className="openMenu"><img alt="" src={menuImg} /></div>}
+            menuCloseButton={<div className="openMenu"><img alt="" src={menuImg} /></div>}
+            changeMenuOn="1175px"
+            largeMenuClassName="large-menu-classname"
+            smallMenuClassName="small-menu-classname"
+            menu={
+              <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/social">Social</Link>
+                <Link to="/gallery">Gallery</Link>
+                <Link to="/workshops">Workshops</Link>
+                <Link to="/store">Store</Link>
+                <Link to="/contact">Contact</Link>
+                <Link to="/blog">Blog</Link>
+              </div>
+            }
+          />
         </nav>
+        <div className="content">
           <Routes />
         </div>
-     
+      </div>
     </Router>
+  </ParallaxProvider>
 )
 export default hot(module)(App)
