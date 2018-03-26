f2016cfa1cf28f6c2bf2e19ab524fea2e5c3aa25
MEGANET\Brett.Hamm <brett.hamm@megasys.com>
2018-03-26 15:19:25 -0600

    Footer 'done'

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,10 +1,14 @@
 
 import React from 'react'
+import doggo1 from 'images/doggo1.jpg'
 //
 
 export default () => (
-  <div className="aboutBackground">
-    <h1>About me</h1>
-    <p>Photographer in Calgary, Alberta</p>
+  <div className="about-content">
+    <img alt="background" src={doggo1} />
+    <div id="aboutContent">
+      <h1>About me</h1>
+      <p>Photographer in Calgary, Alberta</p>
+    </div>
   </div>
 )
--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Router, Link } from 'react-static'
+
 import twitterIcon from 'images/icons/001-twitter.png'
 import youtubeIcon from 'images/icons/002-youtube.png'
 import facebookIcon from 'images/icons/003-facebook.png'
@@ -7,24 +9,37 @@ import fiveHundredIcon from 'images/icons/005-500px-logo.png'
 //
 
 export default () => (
-  <footer className="flex-rw">
-    <ul className="footer-list-top">
-        <h1></h1>
-        <li>TestList</li>
-        <li>TestList</li>
-    </ul>
-    <ul className="footer-list-top">
-        <li>TestList</li>
-        <li>TestList</li>         
-    </ul>
-    <ul className="social-footer">
-        <li><img alt="facebook" src={facebookIcon} /></li>
-        <li><img alt="youtube" src={youtubeIcon} /></li>
-        <li><img alt="500px" src={fiveHundredIcon} /></li>
-        <li><img alt="instagram" src={instagramIcon} /></li>
-        {/* <li><img alt="twitter" src={twitterIcon} /></li> */}
-        
-    </ul>
-
+  <footer>
+    <div  className="footer-grid">
+        <ul className="footer-list" id="footer-left">
+            <h1>Links</h1>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/social">Social</Link>
+            <Link to="/gallery">Gallery</Link>
+            <Link to="/workshops">Workshops</Link>
+            <Link to="/store">Store</Link>
+            <Link to="/contact">Contact</Link>
+            <Link to="/blog">Blog</Link>
+        </ul>
+        <ul className="social-footer"  id="footer-middle">
+            <h1>Connect With Me</h1>
+            <li><img alt="facebook" src={facebookIcon} /></li>
+            <li><img alt="youtube" src={youtubeIcon} /></li>
+            <li><img alt="500px" src={fiveHundredIcon} /></li>
+            <li><img alt="instagram" src={instagramIcon} /></li>
+            <p>irenerudnykphoto@gmail.com</p>
+            {/* <li><img alt="twitter" src={twitterIcon} /></li> */}   
+        </ul>
+    
+        <ul className="footer-list" id="footer-right">
+            <h1>Get Inspired</h1>
+            <li>KelbyOne Link</li>
+            <li>Other Tutorial Link</li>         
+        </ul>
+    </div>
+    <div className="footer-bottom">
+        <h3>Irene Rudnyk Photography                        Copyright 2018 &copy;</h3>
+    </div>
   </footer>
 )
